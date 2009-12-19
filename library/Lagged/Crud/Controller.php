d802731cd39eaa4c94c0ffc532bb2f4d56e75a3e
Till Klampaeckel <till@php.net>
2009-12-20 00:52:41 +0100

    ideas for a crudcontroller

--- library/Lagged/Crud/Controller.php
+++ library/Lagged/Crud/Controller.php
@@ -3,12 +3,19 @@ class Lagged_Crud_Controller extends Zend_Controller_Action
 {
     protected $model;
 
+    protected $primaryKey;
+
     public function init()
     {
+        // when you extend, you need to put your model here
+        // $this->model = new Lagged_Crud_Form;
+
+        $this->primaryKey = $this->model->getPrimaryKey();
     }
 
     public function createAction()
     {
+        
     }
 
     public function deleteAction()
@@ -17,9 +24,37 @@ class Lagged_Crud_Controller extends Zend_Controller_Action
 
     public function readAction()
     {
+        $id = $this->_request->getParam('id', null);
+
+        if ($id === null) {
+            $records = $this->model->fetchAll();
+            $this->view->assign('records', $records->toArray());
+        } else {
+            $record = $this->model->fetchRow("{$this->primaryKey} = ?", $id);
+            $this->view->assign('record', $record->toArray());
+        }
     }
 
     public function updateAction()
     {
+        $id = $this->_request->getParam('id');
+        if ($this->_request->isPost() !== true) {
+            $form = $this->model->getForm($id);
+            $this->view->assign('form', $form);
+            return;
+        }
+
+        $form = $this->model->getForm();
+        $form->populate($this->_request->getPost());
+        if (!$form->isValid()) {
+            $this->view->assign('form', $form);
+            return;
+        }
+
+        $this->model->update(
+            $this->_request->getPost(),
+            "{$this->primarykey} = ?", $id
+        );
+        // redirect to read
     }
 }
