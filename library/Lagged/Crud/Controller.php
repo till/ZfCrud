9ebc595b2b7ea1503bdf288000b9448cb76ca6f0
Till Klampaeckel <till@php.net>
2009-12-20 00:37:57 +0100

    controller

---
+++ library/Lagged/Crud/Controller.php
@@ -0,0 +1,25 @@
+<?php
+class Lagged_Crud_Controller extends Zend_Controller_Action
+{
+    protected $model;
+
+    public function init()
+    {
+    }
+
+    public function createAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+
+    public function readAction()
+    {
+    }
+
+    public function updateAction()
+    {
+    }
+}
