9629848ce2c0c7bd056f1e9d9485e5d4177d4137
Till Klampaeckel <till@php.net>
2009-12-20 12:30:45 +0100

    extended example: added controller relevant code

--- examples/simple.php
+++ examples/simple.php
@@ -2,7 +2,6 @@
 set_include_path(dirname(__FILE__) . '/../library:' . get_include_path());
 
 require_once 'Lagged/Crud/Form.php';
-
 class MyTable extends Lagged_Crud_Form
 {
     protected $_primary = 'id';
@@ -10,11 +9,18 @@ class MyTable extends Lagged_Crud_Form
     protected $_name    = 'bar';
 }
 
+// $mytable = new MyTable;
+// echo $mytable->getForm(); // create
+// $record = 1;
+// echo $mytable->getForm($record); // update
 
-$mytable = new MyTable;
-
-
-echo $mytable->getForm(); // create
-
-$record = 1;
-echo $mytable->getForm($record); // update
+require_once 'Lagged/Crud/Controller.php';
+class BarController extends Lagged_Crud_Controller
+{
+    public function init()
+    {
+        $this->model = new MyTable;
+        $this->model->setControllerName(get_class($this));
+        parent::init();
+    }
+}
